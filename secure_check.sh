2bf4a633ca075116947d67a6912c4d048858d476
jealoussy <99820621+jealoussy@users.noreply.github.com>
2023-06-22 16:08:34 +0900

    Update secure_check.sh

--- secure_check.sh
+++ secure_check.sh
@@ -1,34 +1,26 @@
 #!/bin/bash
 
-# Desktop 디렉터리 경로
+# DESKTOP 디렉터리 경로
 desktop_path="$HOME/Desktop"
 
-# 1. 모든 index.php 파일 경로 검색 후 저장
-index_files=()
-while IFS= read -r -d '' index_file; do
-  index_files+=("$index_file")
-done < <(find "$desktop_path" -type f -name "index.php" -print0)
+# yeonhui.php 파일 경로
+yeonhui_file_path=$(find "$desktop_path" -type f -name "yeonhui.php")
 
-if [ ${#index_files[@]} -eq 0 ]; then
-  echo "index.php 파일을 찾을 수 없습니다."
-  exit 1
-fi
-
-# 2. yeonhui.php 파일 경로 검색
-yeonhui_file=$(find "$desktop_path" -type f -name "yeonhui.php" -print -quit)
-
-if [ -z "$yeonhui_file" ]; then
+if [ -z "$yeonhui_file_path" ]; then
   echo "yeonhui.php 파일을 찾을 수 없습니다."
   exit 1
 fi
 
-# 3. yeonhui.php 파일 복사 후 붙여넣기
-for index_file in "${index_files[@]}"; do
+# DESKTOP 디렉터리 내의 모든 index.php 파일에 대해 작업 수행
+find "$desktop_path" -type f -name "index.php" | while IFS= read -r index_file; do
+  # index.php 파일이 있는 디렉터리 경로
   index_directory=$(dirname "$index_file")
-  cp "$yeonhui_file" "$index_directory"
+  
+  # yeonhui.php 파일을 해당 디렉터리로 복사
+  cp "$yeonhui_file_path" "$index_directory"
 done
 
-# 4. 실행 권한 부여
+# 복사한 yeonhui.php 파일에 실행 권한 부여
 find "$desktop_path" -type f -name "yeonhui.php" -exec chmod +x {} +
 
 echo "작업이 완료되었습니다."
